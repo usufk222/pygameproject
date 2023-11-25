373a3d42edbafb55bc95b297db4122089c43593e
emaan-naveed <151548701+emaan-naveed@users.noreply.github.com>
2023-11-25 17:37:28 +0500

    Update emaancode.py

--- pygameproject/emaancode.py
+++ pygameproject/emaancode.py
@@ -1,1 +1,24 @@
 import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Working With Rectangles")
+
+rect_1 = pygame.Rect(200, 100, 150, 100)
+
+run = True
+while run:
+
+  print(rect_1)
+
+  for event in pygame.event.get():
+   if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.flip()
+
+pygame.quit()
