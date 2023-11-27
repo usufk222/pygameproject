52077d26b0e3e876c042d4e2ed25c194ac90faf4
emaan-naveed <151548701+emaan-naveed@users.noreply.github.com>
2023-11-27 22:30:39 +0500

    Update emaancode.py

--- pygameproject/emaancode.py
+++ pygameproject/emaancode.py
@@ -6,19 +6,37 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Working With Rectangles")
+pygame.display.set_caption("Moving Rectangle")
 
-rect_1 = pygame.Rect(200, 100, 150, 100)
+rect_2 = pygame.Rect(200, 200, 50, 50)  # Create a rectangle
+
+clock = pygame.time.Clock()
 
 run = True
 while run:
+    clock.tick(60)
 
-  print(rect_1)
+    # Fill the background white
+    screen.fill((255, 255, 255))
 
-  for event in pygame.event.get():
-   if event.type == pygame.QUIT:
-      run = False
+    # Player controls
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        rect_2.x -= 5
+    if keys[pygame.K_d]:
+        rect_2.x += 5
+    if keys[pygame.K_w]:
+        rect_2.y -= 5
+    if keys[pygame.K_s]:
+        rect_2.y += 5
 
-  pygame.display.flip()
+    # Draw the rectangle
+    pygame.draw.rect(screen, (0, 255, 255), rect_2)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
 
 pygame.quit()
